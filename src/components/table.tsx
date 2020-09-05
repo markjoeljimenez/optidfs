e604511e4a8de162d1e230f764f24c5940ad15f6
markjoeljimenez <markjoeljimenez@gmail.com>
2020-09-04 23:55:17 -0400

    Fix row border style

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -352,70 +352,68 @@ const Table = ({
 						>
 							{totalFppg}
 						</div>
-						<div className="table__cell " role="cell" />
+						<div
+							className="table__cell table__cell--options table__cell--align-center"
+							role="cell"
+						/>
 					</div>
 
 					{lineups.length > 1 && (
-						<div className="table__row-group" role="rowgroup">
-							<div
-								className="table__row table__row--child"
-								role="cell"
-							>
-								<div className="table__cell table__cell--pagination">
-									<button
-										type="button"
-										onClick={() => previous()}
+						<div
+							className="table__row table__row--child"
+							role="cell"
+						>
+							<div className="table__cell table__cell--pagination">
+								<button
+									type="button"
+									onClick={() => previous()}
+								>
+									Previous
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 24 24"
+										width="24"
+										height="24"
 									>
-										Previous
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											viewBox="0 0 24 24"
-											width="24"
-											height="24"
-										>
-											<g data-name="Layer 2">
-												<g data-name="arrow-ios-back">
-													<rect
-														width="24"
-														height="24"
-														transform="rotate(90 12 12)"
-														opacity="0"
-													/>
-													<path d="M13.83 19a1 1 0 0 1-.78-.37l-4.83-6a1 1 0 0 1 0-1.27l5-6a1 1 0 0 1 1.54 1.28L10.29 12l4.32 5.36a1 1 0 0 1-.78 1.64z" />
-												</g>
+										<g data-name="Layer 2">
+											<g data-name="arrow-ios-back">
+												<rect
+													width="24"
+													height="24"
+													transform="rotate(90 12 12)"
+													opacity="0"
+												/>
+												<path d="M13.83 19a1 1 0 0 1-.78-.37l-4.83-6a1 1 0 0 1 0-1.27l5-6a1 1 0 0 1 1.54 1.28L10.29 12l4.32 5.36a1 1 0 0 1-.78 1.64z" />
 											</g>
-										</svg>
-									</button>
+										</g>
+									</svg>
+								</button>
 
-									{`${page + 1} of ${
-										lineups.length
-									} generated lineups`}
+								{`${page + 1} of ${
+									lineups.length
+								} generated lineups`}
 
-									<button
-										type="button"
-										onClick={() => next()}
+								<button type="button" onClick={() => next()}>
+									Next
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 24 24"
+										width="24"
+										height="24"
 									>
-										Next
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											viewBox="0 0 24 24"
-											width="24"
-											height="24"
-										>
-											<g data-name="Layer 2">
-												<g data-name="arrow-ios-forward">
-													<rect
-														width="24"
-														height="24"
-														transform="rotate(-90 12 12)"
-														opacity="0"
-													/>
-													<path d="M10 19a1 1 0 0 1-.64-.23 1 1 0 0 1-.13-1.41L13.71 12 9.39 6.63a1 1 0 0 1 .15-1.41 1 1 0 0 1 1.46.15l4.83 6a1 1 0 0 1 0 1.27l-5 6A1 1 0 0 1 10 19z" />
-												</g>
+										<g data-name="Layer 2">
+											<g data-name="arrow-ios-forward">
+												<rect
+													width="24"
+													height="24"
+													transform="rotate(-90 12 12)"
+													opacity="0"
+												/>
+												<path d="M10 19a1 1 0 0 1-.64-.23 1 1 0 0 1-.13-1.41L13.71 12 9.39 6.63a1 1 0 0 1 .15-1.41 1 1 0 0 1 1.46.15l4.83 6a1 1 0 0 1 0 1.27l-5 6A1 1 0 0 1 10 19z" />
 											</g>
-										</svg>
-									</button>
-								</div>
+										</g>
+									</svg>
+								</button>
 							</div>
 						</div>
 					)}
