d4fe463e3c08ffd0dc12dfccdbcb531c69cf44b4
markjoeljimenez <markjoeljimenez@gmail.com>
2020-10-03 23:40:02 -0400

    Update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,25 @@
 module.exports = {
-	purge: [],
+	purge: ['./src/**/*.tsx'],
+	future: {
+		removeDeprecatedGapUtilities: true,
+		purgeLayersByDefault: true,
+	},
 	theme: {
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+		},
+		colors: {
+			transparent: 'transparent',
+			black: '#000',
+			white: '#fff',
+			grey: {
+				100: '#f7fafc',
+				900: '#1a202c',
+			},
+		},
 		extend: {},
 	},
 	variants: {},
