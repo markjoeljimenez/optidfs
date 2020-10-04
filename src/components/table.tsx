1088c35eed3e73aedf59d770030efa6db3d713e8
markjoeljimenez <markjoeljimenez@gmail.com>
2020-10-04 13:39:43 -0400

    Fix total and pagination in table

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -130,7 +130,7 @@ const Table = ({
 					>
 						FPPG
 					</div>
-					<div className="p-2 flex-1" role="columnheader">
+					<div className="p-2" role="columnheader">
 						<span hidden>Options</span>
 					</div>
 				</div>
@@ -383,96 +383,95 @@ const Table = ({
 			</div>
 
 			{lineups && (
-				<div className="table__row-group table__footer" role="rowgroup">
-					<div className="table__row" role="row">
-						<div className="table__row-group table__row-group--main">
-							<div className="table__row">
-								<div
-									className="table__cell table__cell--total"
-									role="cell"
-								>
-									Total
-								</div>
-								<div
-									className="table__cell table__cell--salary table__cell--align-right"
-									role="cell"
-								>
-									{totalSalary &&
-										new Intl.NumberFormat('en-US', {
-											style: 'currency',
-											currency: 'USD',
-											minimumFractionDigits: 0,
-										}).format(totalSalary)}
-								</div>
-								<div
-									className="table__cell table__cell--fppg table__cell--align-right"
-									role="cell"
-								>
-									{totalFppg}
-								</div>
-							</div>
+				<div className="border-b border-gray-300" role="rowgroup">
+					<div
+						className="grid gap-2 md:gap-0 grid-cols-table-md font-black container mx-auto px-8"
+						role="row"
+					>
+						<div className="p-2 py-5" role="cell">
+							Total
+						</div>
+						<div
+							className="p-2 py-5 flex items-center justify-end col-start-4 md:col-start-7"
+							role="cell"
+						>
+							{totalSalary &&
+								new Intl.NumberFormat('en-US', {
+									style: 'currency',
+									currency: 'USD',
+									minimumFractionDigits: 0,
+								}).format(totalSalary)}
+						</div>
+						<div
+							className="p-2 py-5 flex items-center justify-end"
+							role="cell"
+						>
+							{totalFppg}
 						</div>
 					</div>
+
 					{lineups.length > 1 && (
-						<div className="table__row" role="cell">
-							<div className="table__row-group table__row-group--main">
-								<div className="table__row">
-									<div className="table__cell table__cell--pagination">
-										<button
-											type="button"
-											onClick={() => previous()}
-										>
-											Previous
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												viewBox="0 0 24 24"
-												width="24"
-												height="24"
-											>
-												<g data-name="Layer 2">
-													<g data-name="arrow-ios-back">
-														<rect
-															width="24"
-															height="24"
-															transform="rotate(90 12 12)"
-															opacity="0"
-														/>
-														<path d="M13.83 19a1 1 0 0 1-.78-.37l-4.83-6a1 1 0 0 1 0-1.27l5-6a1 1 0 0 1 1.54 1.28L10.29 12l4.32 5.36a1 1 0 0 1-.78 1.64z" />
-													</g>
-												</g>
-											</svg>
-										</button>
+						<div
+							role="row"
+							className="grid grid-cols-table-md font-black border-t border-gray-300"
+						>
+							<div
+								className="p-2 py-5 flex items-center justify-between col-span-9 container mx-auto px-8 "
+								role="cell"
+							>
+								<button
+									type="button"
+									onClick={() => previous()}
+								>
+									<span className="sr-only">Previous</span>
 
-										{`${page + 1} of ${
-											lineups.length
-										} generated lineups`}
+									<svg
+										className="fill-current"
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 24 24"
+										width="24"
+										height="24"
+									>
+										<g data-name="Layer 2">
+											<g data-name="arrow-ios-back">
+												<rect
+													width="24"
+													height="24"
+													transform="rotate(90 12 12)"
+													opacity="0"
+												/>
+												<path d="M13.83 19a1 1 0 0 1-.78-.37l-4.83-6a1 1 0 0 1 0-1.27l5-6a1 1 0 0 1 1.54 1.28L10.29 12l4.32 5.36a1 1 0 0 1-.78 1.64z" />
+											</g>
+										</g>
+									</svg>
+								</button>
 
-										<button
-											type="button"
-											onClick={() => next()}
-										>
-											Next
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												viewBox="0 0 24 24"
-												width="24"
-												height="24"
-											>
-												<g data-name="Layer 2">
-													<g data-name="arrow-ios-forward">
-														<rect
-															width="24"
-															height="24"
-															transform="rotate(-90 12 12)"
-															opacity="0"
-														/>
-														<path d="M10 19a1 1 0 0 1-.64-.23 1 1 0 0 1-.13-1.41L13.71 12 9.39 6.63a1 1 0 0 1 .15-1.41 1 1 0 0 1 1.46.15l4.83 6a1 1 0 0 1 0 1.27l-5 6A1 1 0 0 1 10 19z" />
-													</g>
-												</g>
-											</svg>
-										</button>
-									</div>
-								</div>
+								{`${page + 1} of ${
+									lineups.length
+								} generated lineups`}
+
+								<button type="button" onClick={() => next()}>
+									<span className="sr-only">Next</span>
+									<svg
+										className="fill-current"
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 24 24"
+										width="24"
+										height="24"
+									>
+										<g data-name="Layer 2">
+											<g data-name="arrow-ios-forward">
+												<rect
+													width="24"
+													height="24"
+													transform="rotate(-90 12 12)"
+													opacity="0"
+												/>
+												<path d="M10 19a1 1 0 0 1-.64-.23 1 1 0 0 1-.13-1.41L13.71 12 9.39 6.63a1 1 0 0 1 .15-1.41 1 1 0 0 1 1.46.15l4.83 6a1 1 0 0 1 0 1.27l-5 6A1 1 0 0 1 10 19z" />
+											</g>
+										</g>
+									</svg>
+								</button>
 							</div>
 						</div>
 					)}
