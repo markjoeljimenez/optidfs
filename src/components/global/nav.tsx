9682df0bdb52482ddf217f84309e25e5b4a0f711
markjoeljimenez <markjoeljimenez@gmail.com>
2020-10-08 19:46:13 -0400

    Fix donate on mobile

--- src/components/global/nav.tsx
+++ src/components/global/nav.tsx
@@ -23,171 +23,124 @@ const Nav = (props: any) => {
 	};
 
 	return (
-		<nav className="bg-gray-100 px-6 py-4 md:py-8 border-r border-gray-200 font-bold flex flex-col justify-between">
-			<div>
-				<div className="relative flex justify-between items-center md:block">
-					<button
-						onClick={onClick}
-						type="button"
-						className="md:hidden"
-					>
-						{isActive ? (
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 24 24"
-								width="24"
-								height="24"
-							>
-								<g data-name="Layer 2">
-									<g data-name="close">
-										<rect
-											width="24"
-											height="24"
-											transform="rotate(180 12 12)"
-											opacity="0"
-										/>
-										<path d="M13.41 12l4.3-4.29a1 1 0 1 0-1.42-1.42L12 10.59l-4.29-4.3a1 1 0 0 0-1.42 1.42l4.3 4.29-4.3 4.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l4.29-4.3 4.29 4.3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42z" />
-									</g>
+		<nav className="bg-gray-100 px-6 py-4 md:py-8 border-r border-gray-200 font-bold flex flex-col">
+			<div className="relative flex justify-between items-center md:block">
+				<button onClick={onClick} type="button" className="md:hidden">
+					{isActive ? (
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							width="24"
+							height="24"
+						>
+							<g data-name="Layer 2">
+								<g data-name="close">
+									<rect
+										width="24"
+										height="24"
+										transform="rotate(180 12 12)"
+										opacity="0"
+									/>
+									<path d="M13.41 12l4.3-4.29a1 1 0 1 0-1.42-1.42L12 10.59l-4.29-4.3a1 1 0 0 0-1.42 1.42l4.3 4.29-4.3 4.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l4.29-4.3 4.29 4.3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42z" />
 								</g>
-							</svg>
-						) : (
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 24 24"
-								width="24"
-								height="24"
-							>
-								<g data-name="Layer 2">
-									<g data-name="menu">
-										<rect
-											width="24"
-											height="24"
-											transform="rotate(180 12 12)"
-											opacity="0"
-										/>
-										<rect
-											x="3"
-											y="11"
-											width="18"
-											height="2"
-											rx=".95"
-											ry=".95"
-										/>
-										<rect
-											x="3"
-											y="16"
-											width="18"
-											height="2"
-											rx=".95"
-											ry=".95"
-										/>
-										<rect
-											x="3"
-											y="6"
-											width="18"
-											height="2"
-											rx=".95"
-											ry=".95"
-										/>
-									</g>
+							</g>
+						</svg>
+					) : (
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							width="24"
+							height="24"
+						>
+							<g data-name="Layer 2">
+								<g data-name="menu">
+									<rect
+										width="24"
+										height="24"
+										transform="rotate(180 12 12)"
+										opacity="0"
+									/>
+									<rect
+										x="3"
+										y="11"
+										width="18"
+										height="2"
+										rx=".95"
+										ry=".95"
+									/>
+									<rect
+										x="3"
+										y="16"
+										width="18"
+										height="2"
+										rx=".95"
+										ry=".95"
+									/>
+									<rect
+										x="3"
+										y="6"
+										width="18"
+										height="2"
+										rx=".95"
+										ry=".95"
+									/>
 								</g>
-							</svg>
-						)}
-						<span className="sr-only">Menu</span>
-					</button>
-					{/* <picture className="block max-w-logo-sm md:max-w-logo-md absolute ml-10 md:ml-0 md:relative">
+							</g>
+						</svg>
+					)}
+					<span className="sr-only">Menu</span>
+				</button>
+				{/* <picture className="block max-w-logo-sm md:max-w-logo-md absolute ml-10 md:ml-0 md:relative">
 						<img
 							className="block"
 							src="/images/fortify.svg"
 							alt="Fortify logo"
 						/>
 					</picture> */}
-					<h1 className="absolute ml-10 md:ml-0 md:relative md:text-3xl font-black whitespace-no-wrap">
-						DK Optimizer
-					</h1>
-					<div className="relative md:mt-8 md:mb-6">
-						<label htmlFor="selectSport">
-							<span className="sr-only">Select Sport</span>
-							<select
-								onChange={handleSportChange}
-								className="font-bold cursor-pointer shadow appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-xs md:text-base"
-								id="selectSport"
-							>
-								{sports.map((sport) => (
-									<option value={sport} key={sport}>
-										{sport}
-									</option>
-								))}
-							</select>
-						</label>
+				<h1 className="absolute ml-10 md:ml-0 md:relative md:text-3xl font-black whitespace-no-wrap">
+					DK Optimizer
+				</h1>
+				<div className="relative md:mt-8 md:mb-6">
+					<label htmlFor="selectSport">
+						<span className="sr-only">Select Sport</span>
+						<select
+							onChange={handleSportChange}
+							className="font-bold cursor-pointer shadow appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-xs md:text-base"
+							id="selectSport"
+						>
+							{sports.map((sport) => (
+								<option value={sport} key={sport}>
+									{sport}
+								</option>
+							))}
+						</select>
+					</label>
 
-						<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-							<svg
-								className="fill-current"
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 24 24"
-								width="24"
-								height="24"
-							>
-								<g data-name="Layer 2">
-									<g data-name="chevron-down">
-										<rect
-											width="24"
-											height="24"
-											opacity="0"
-										/>
-										<path d="M12 15.5a1 1 0 0 1-.71-.29l-4-4a1 1 0 1 1 1.42-1.42L12 13.1l3.3-3.18a1 1 0 1 1 1.38 1.44l-4 3.86a1 1 0 0 1-.68.28z" />
-									</g>
+					<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+						<svg
+							className="fill-current"
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							width="24"
+							height="24"
+						>
+							<g data-name="Layer 2">
+								<g data-name="chevron-down">
+									<rect width="24" height="24" opacity="0" />
+									<path d="M12 15.5a1 1 0 0 1-.71-.29l-4-4a1 1 0 1 1 1.42-1.42L12 13.1l3.3-3.18a1 1 0 1 1 1.38 1.44l-4 3.86a1 1 0 0 1-.68.28z" />
 								</g>
-							</svg>
-						</div>
+							</g>
+						</svg>
 					</div>
 				</div>
-				<ul
-					className={clsx(
-						'md:max-h-none overflow-hidden transition-all duration-300',
-						isActive ? 'max-h-10' : 'max-h-0'
-					)}
-				>
-					{/* <li className="mt-8 mb-6">
-						<div className="relative">
-							<label htmlFor="selectSport">
-								<span className="sr-only">Select Sport</span>
-								<select
-									onChange={handleSportChange}
-									className="font-bold cursor-pointer shadow appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-									id="selectSport"
-								>
-									{sports.map((sport) => (
-										<option value={sport} key={sport}>
-											{sport}
-										</option>
-									))}
-								</select>
-							</label>
-
-							<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-								<svg
-									className="fill-current"
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 24 24"
-									width="24"
-									height="24"
-								>
-									<g data-name="Layer 2">
-										<g data-name="chevron-down">
-											<rect
-												width="24"
-												height="24"
-												opacity="0"
-											/>
-											<path d="M12 15.5a1 1 0 0 1-.71-.29l-4-4a1 1 0 1 1 1.42-1.42L12 13.1l3.3-3.18a1 1 0 1 1 1.38 1.44l-4 3.86a1 1 0 0 1-.68.28z" />
-										</g>
-									</g>
-								</svg>
-							</div>
-						</div>
-					</li> */}
+			</div>
+			<div
+				className={clsx(
+					'md:max-h-none overflow-hidden transition-all duration-300 flex-1 flex flex-col justify-between space-y-6',
+					isActive ? 'max-h-10' : 'max-h-0'
+				)}
+			>
+				<ul>
 					<li className="mt-6 md:mt-0">
 						<Link href="/">
 							<a
@@ -260,10 +213,9 @@ const Nav = (props: any) => {
                                 </Link>
                             </li> */}
 				</ul>
-			</div>
-
-			<div className="flex justify-center">
-				<Donate />
+				<div className="flex justify-center">
+					<Donate />
+				</div>
 			</div>
 		</nav>
 	);
