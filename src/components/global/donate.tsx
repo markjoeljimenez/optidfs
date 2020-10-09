d38b65cb193aa6b9ef3a2ce5eb9b06237eca71c8
markjoeljimenez <markjoeljimenez@gmail.com>
2020-10-08 21:14:10 -0400

    Fix build errors

--- src/components/global/donate.tsx
+++ src/components/global/donate.tsx
@@ -11,14 +11,12 @@ const Donate = () => (
 		<input
 			type="image"
 			src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif"
-			border="0"
 			name="submit"
 			title="PayPal - The safer, easier way to pay online!"
 			alt="Donate with PayPal button"
 		/>
 		<img
 			alt=""
-			border="0"
 			src="https://www.paypal.com/en_CA/i/scr/pixel.gif"
 			width="1"
 			height="1"
