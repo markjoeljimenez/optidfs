242a950da932fe0978a8630b22ac6f88ce952ab2
Mark Jimenez <markjoeljimenez@gmail.com>
2022-08-07 21:01:53 -0400

    Add Yahoo export

--- src/containers/Export/services/mapPlayersToCSV.ts
+++ src/containers/Export/services/mapPlayersToCSV.ts
@@ -1,13 +1,41 @@
-import { IGetOptimizedLineupsResponse } from '@/containers/Optimize';
+import { IContest } from '@/containers/Contests';
+import {
+	IGetOptimizedLineupsResponse,
+	IOptimizedLineup,
+} from '@/containers/Optimize';
+
+function mapIds(lineups: IOptimizedLineup[]) {
+	return lineups.map((lineup) => lineup.players.map((player) => player.id));
+}
 
 export function mapPlayersToDraftkingsCSV(
 	response: IGetOptimizedLineupsResponse | undefined
 ) {
 	if (response) {
-		const playerIds = response?.lineups.map((lineup) =>
-			lineup.players.map((player) => player.id)
-		);
+		const playerIds = mapIds(response.lineups);
 
 		return [response.positions, ...playerIds];
+	}
+}
+
+export function mapPlayersToYahooCSV(
+	response: IGetOptimizedLineupsResponse | undefined,
+	selectedContest: IContest
+) {
+	if (response) {
+		const playerIds = mapIds(response.lineups);
+
+		const transformedPositionHeaders = [
+			'Contest ID',
+			'Entry ID',
+			...response.positions,
+		];
+		const transformedPlayerIds = playerIds.map((players) => [
+			selectedContest.contest_id,
+			undefined,
+			...players,
+		]);
+
+		return [transformedPositionHeaders, ...transformedPlayerIds];
 	}
 }
