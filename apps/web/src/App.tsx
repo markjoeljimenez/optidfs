c99bc320f15f799b3210ace54eb8ab511e1842c5
markjoeljimenez <markjoeljimenez@gmail.com>
2023-09-28 14:02:25 +0200

    Add mantine

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,34 @@
+import { AppShell, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
+export function App() {
+	const [opened, { toggle }] = useDisclosure();
+
+	return (
+		<AppShell
+			header={{ height: 60 }}
+			navbar={{
+				width: 300,
+				breakpoint: 'sm',
+				collapsed: { mobile: !opened },
+			}}
+			padding="md"
+		>
+			<AppShell.Header>
+				<Burger
+					opened={opened}
+					onClick={toggle}
+					hiddenFrom="sm"
+					size="sm"
+				/>
+				<div>Logo</div>
+			</AppShell.Header>
+
+			<AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+
+			<AppShell.Main>Main</AppShell.Main>
+		</AppShell>
+	);
+}
+
+export default App;
---
+++ apps/web/src/index.tsx
@@ -0,0 +1,17 @@
+import '@mantine/core/styles.css';
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
+import { MantineProvider } from '@mantine/core';
+import { StrictMode } from 'react';
+
+const root = ReactDOM.createRoot(
+	document.getElementById('root') as HTMLElement
+);
+
+root.render(
+	<StrictMode>
+		<MantineProvider defaultColorScheme="dark">
+			<App />
+		</MantineProvider>
+	</StrictMode>
+);
