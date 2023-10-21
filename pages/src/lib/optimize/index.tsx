53fe46ab7cf6d58e0d42b3d31dce742c1af41026
Mark Jimenez <markjoeljimenez@gmail.com>
2023-10-21 03:01:32 -0400

    Add store lib

--- pages/src/lib/optimize/index.tsx
+++ pages/src/lib/optimize/index.tsx
@@ -1,7 +1,13 @@
+import { useAppSelector } from '@optidfs.rewrite.2/api';
+
 /* eslint-disable-next-line */
 export interface PagesProps {}
 
 export function Optimize(props: PagesProps) {
+	const count = useAppSelector((state) => state.counter.value);
+
+	console.log(count);
+
 	return (
 		<div>
 			<h1>Welcome to Pages!</h1>
---
+++ store/src/lib/store.ts
@@ -0,0 +1,57 @@
+import { OptidfsDFSApi } from '@optidfs.rewrite.2/api';
+import { PayloadAction, configureStore, createSlice } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+
+// Define a type for the slice state
+interface CounterState {
+	value: number;
+}
+
+// Define the initial state using that type
+const initialState: CounterState = {
+	value: 0,
+};
+
+export const counterSlice = createSlice({
+	name: 'counter',
+	// `createSlice` will infer the state type from the `initialState` argument
+	initialState,
+	reducers: {
+		increment: (state) => {
+			state.value += 1;
+		},
+		decrement: (state) => {
+			state.value -= 1;
+		},
+		// Use the PayloadAction type to declare the contents of `action.payload`
+		incrementByAmount: (state, action: PayloadAction<number>) => {
+			state.value += action.payload;
+		},
+	},
+});
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+// Other code such as selectors can use the imported `RootState` type
+export const selectCount = (state: RootState) => state.counter.value;
+
+export const store = configureStore({
+	reducer: {
+		// Add the generated reducer as a specific top-level slice
+		[OptidfsDFSApi.reducerPath]: OptidfsDFSApi.reducer,
+		counter: counterSlice.reducer,
+	},
+	// Adding the api middleware enables caching, invalidation, polling,
+	// and other useful features of `rtk-query`.
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(OptidfsDFSApi.middleware),
+});
+
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+setupListeners(store.dispatch);
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
